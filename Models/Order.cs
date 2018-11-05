c71f06c2d33a6fff47a2919acdc04db56f53a175
victor.kasmin <victor.kasmin@gmail.com>
2018-11-05 23:11:47 +0300

    Change DB to SQLite

--- Models/Order.cs
+++ Models/Order.cs
@@ -22,39 +22,5 @@ namespace Shop.Models
             return $"{DateTime.Now.ToString("yyyyMMdd-HHmmss")}";
         }
 
-        //public override string ToString()
-        //{
-        //    var stringBuilder = new StringBuilder();
-
-        //    stringBuilder.AppendLine($"Заказ №{Number}");
-        //    stringBuilder.AppendLine($"Название | кол-во | стоимость |");
-
-        //    foreach (var item in Items)
-        //    {
-        //        var productNameLength = item.Product.Name.Length;
-
-        //        var productName = item.Product.Name;
-        //        if (productNameLength < 8)
-        //            productName += string.Join(string.Empty, Enumerable.Range(0, 8 - productNameLength).Select(x => " "));
-
-        //        stringBuilder.AppendLine("-----------------------------");
-        //        stringBuilder.AppendLine($"{productName} |   {item.Count}    |      {item.Product.Price}");
-        //    }
-
-        //    stringBuilder.AppendLine("-----------------------------");
-        //    var totalSum = Items.Sum(x => x.Product.Price * x.Count);
-        //    if (Discount != null)
-        //    {
-        //        totalSum = Discount.Apply(totalSum);
-        //        stringBuilder.AppendLine($"Была применена скидка: {Discount}");
-        //        stringBuilder.AppendLine($"Итоговая стоимость со учетом скидки: {totalSum}");
-        //    }
-        //    else
-        //    {
-        //        stringBuilder.AppendLine($"Итоговая стоимость: {totalSum}");
-        //    }
-
-        //    return stringBuilder.ToString();
-        //}
     }
 }
