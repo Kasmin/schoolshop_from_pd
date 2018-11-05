4dd4d83f461cd8127a8c015d24703cdfc75bc72f
victor.kasmin <victor.kasmin@gmail.com>
2018-11-05 23:16:55 +0300

    Add blank Cart model

---
+++ Models/Cart.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Shop.Models
+{
+    public class Cart
+    {
+        public List<Product> Products;
+        public decimal Sum;
+
+        public Cart()
+        {
+        }
+    }
+}
